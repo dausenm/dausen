856414b419ba1c50559ffe8efe9c97247942d727
dausen <dausen@ubuntu.myguest.virtualbox.org>
2023-01-27 00:35:52 -0600

    almost done with part 1

--- school/cs300/p1/dmmason2_part1.c
+++ school/cs300/p1/dmmason2_part1.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 #define MAX_LINE 80 /* The maximum length command */
 
-char **parseArgs(char *command);
+int parseArgs(char *command, char *args[]);
+
+void addToArgs(char buf[], char *args[], int argc, int count);
 
 int main(void){
     char *args[MAX_LINE/2 + 1]; /* command line arguments */
@@ -14,64 +17,102 @@ int main(void){
         fflush(stdout);
 
         char command[100];
-    /**
-    * After reading user input, the steps are:
-    * (1) fork a child process using fork()
-    * */
-    fgets(command, MAX_LINE, stdin);
 
-    //parse args func to get args[][]?
+        fgets(command, MAX_LINE, stdin);
 
-    char **args = parseArgs(command);
-
-    printf("returned from parseArgs\n");
-
-    pid = fork();
-
-    //free(args);
-
-    int w = 1;
-
-
-    if(pid == 0) {
-        printf("child was created! wooooo!\n");
-        printf("%s\n", command);
-    }
-
-    else if(pid > 0) {
-        printf("parent is still executing! woooo!\n");
-
-        if(w){
-            printf("parent waiting\n");
-            wait();
+        if(!strcmp(command, "exit")){
+            printf("exiting\n");
+            should_run = 0;
+            return 0;
+            break;
         }
-    }
 
-    else if (pid == -1){
-        printf("something went wrong. fuck.\n");
-    }
+        char *args[MAX_LINE/2];
 
+        int w = parseArgs(command, args);
 
-   /*
-    * (2) the child process will invoke execvp()
+        /**
+        * After reading user input, the steps are:
+        * (1) fork a child process using fork()
+        * */
 
-    */
+        pid = fork();
 
-   /*
-    * (3) if command included &, parent will invoke wait()
-    */
+        if(pid == 0) {      //(2) the child process will invoke execvp()
+            printf("child was created! wooooo!\n");
+            printf("%s\n", command);
+            execvp(args[0], args);
+            exit(0);
+        }
+
+        else if(pid > 0) {      //(3) if command included &, parent will invoke wait()
+            printf("parent is still executing! woooo!\n");
+
+            if(w){
+                printf("parent waiting\n");
+                wait();
+            }
+        }
+
+        else if (pid == -1){
+            printf("something went wrong. fuck.\n");
+        }
     }
 
     return 0;
 }
 
-char **parseArgs(char *command){
-    printf("in parseArgs\n");
-    char **rt;
+int parseArgs(char *command, char *args[]){        //example input: ""
+    char buf[MAX_LINE];
 
-    //rt[0][0] = 'a';
+    int stop = 0;
+    int count = 0;
+    int argc = 0;
+    int rt = 0;
 
-    printf("exiting\n");
+    for(int i = 0; i < MAX_LINE; i++){
+        if(command[i] != ' ' && command[i] != '\0'){
+            stop = 0;
+            buf[count] = command[i];
+            count++;
+        }
+
+        if(stop){
+            break;
+        }
+
+        if(command[i] == ' ' || command[i] == '\0'){
+            if(!strcmp(buf, "&")){
+                rt = 1;
+                stop = 1;
+                continue;
+            }
+            //add buf to args, increment argc
+            addToArgs(buf, args, argc, count);
+            argc++;
+
+            //clear buf and reset count;
+            count = 0;
+            strcpy(buf, "");
+
+            //finally, set stop to 1;
+            stop = 1;
+        }
+    }
 
     return rt;
+}
+
+
+void addToArgs(char buf[], char *args[], int argc, int count){
+    char *buffer;
+
+    buffer = (char *) malloc(count * sizeof(char));
+
+    for(int i = 0; i < count; i++){
+        buffer[i] = buf[i];
+    }
+
+    args[argc] = buffer;
+    return;
 }
