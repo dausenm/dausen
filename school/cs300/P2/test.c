13142c572bb1c4b103dff28d45c85cc2ad0c1bba
dausenm <dausen@ubuntu.myguest.virtualbox.org>
2023-02-24 12:57:29 -0600

    almost added read functionality

--- school/cs300/P2/test.c
+++ school/cs300/P2/test.c
@@ -11,16 +11,16 @@ int main(int argc, char* argv[])
 {
 	FILE *fp;
 	
-	if(argc<2){
+	if(argc < 2){
 		printf("Error: Use ./a.out filename.txt\n");
-		return 0;
+		return 1;
 	}
 
 	fp = fopen(argv[1], "r");
 
-	if(fp==NULL){
+	if(fp == NULL){
 		printf("Error: File not found: \"%s\"\n", argv[1]);
-		return 0;
+		return 1;
 	}
 
 	char *value = NULL;
@@ -41,13 +41,13 @@ int main(int argc, char* argv[])
 	int tlb[TLB_SIZE][2];
 	int pagetable[PAGE_SIZE];
 
-	memset(tlb,-1,TLB_SIZE*2*sizeof(tlb[0][0]));
+	memset(tlb, -1, TLB_SIZE * 2 * sizeof(tlb[0][0]));
 	memset(pagetable, -1, sizeof(pagetable));
 	
 	int mask = 255;
-	int mask1 = 62580;
 	int i;
 	int hit;
+	int data;
 	
 	while((read = getline(&value, &len, fp)) != -1){
 		pages++;		//increment pages at the top of the loop, keep track of how many pages we have looped through
@@ -60,7 +60,7 @@ int main(int argc, char* argv[])
 		offset = offset & mask;
 		
 		logAddr = atoi(value);
-		//printf("%lld %lld\n",pageNum,offset);
+
 		frame = 0, physAddr = 0;
 		
 		hit = 0;			//This will be set to 1 if it is found in TLB
@@ -69,46 +69,68 @@ int main(int argc, char* argv[])
 		
 		for(i = 0; i < TLB_SIZE; i++){
 			if(tlb[i][0] == pageNum){
-				hit=1;
+				hit = 1;
 				TLBHits++;
 				frame = tlb[i][1];
+				data = frame;
 				break;
 			}
 		}
+
 		//If not found in TLB, search in page table
 		if(hit != 1){
-			int f = 0;
+			int freeFrame = 0;
 			for(i = 0; i < PAGE_SIZE; i++){
 				if(pagetable[i] == pageNum){
 					frame = i;
 					break;
 				}
 				if(pagetable[i]==-1){
-					f=1;
+					freeFrame = 1;
 					pageFaults++;
 					break;
 				}
 			}
-			if(f==1){
+			if(freeFrame == 1){
 				pagetable[i] = pageNum;
 				frame = i;
 			}
 			//replace in tlb using fifo
 			tlb[q][0] = pageNum;
-			tlb[q][1] = i;
+			
+			//find value from BACKING_STORE.bin
+			FILE *bs = fopen("BACKING_STORE.bin", "rb");
+
+			if(bs == NULL){
+				printf("Error: BACKING_STORE.bin not found.\n");
+				return 1;
+			}
+
+			char buffer[PAGE_SIZE];
+
+			fseek(bs, pageNum * PAGE_SIZE, SEEK_SET);
+			fread(buffer, PAGE_SIZE, 1, bs);
+
+			printf("\t\tD: Buffer = %s\tpageNum = %lld\n", buffer, pageNum);
+			
+			data = atoi(buffer);
+
+			fclose(bs);
+
+			tlb[q][1] = data;
 			q++;
-			q = q % 15;		
+			q = q % 15;
 		}
 
 		physAddr = frame * PAGE_SIZE + offset;
-		printf("Virtual address: %d \t\tPhysical address: %d Value: %d\n", logAddr, physAddr, 0);
+		printf("Virtual address: %d \t\tPhysical address: %d Value: %d\n", logAddr, physAddr, data);
 	}
 	double hitrate = ((double)TLBHits / (double)pages);
 	double faultrate = ((double)pageFaults / (double)pages);
 
     printf("Number of Translated Addresses = %d\n", (int)pages);
     printf("Page Faults = %d\n", (int)pageFaults);
-    printf("Page pageFaults Rate = %.3lf\n", faultrate);
+    printf("Page Fault Rate = %.3lf\n", faultrate);
     printf("TLB Hits = %d\n", (int)TLBHits);
     printf("TLB Hit Rate = %.3lf\n", hitrate);
 }
